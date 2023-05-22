1c3e9ad69dced724428810df84a2769ca90bf1ec
MikeeOo <michal.olszewskki@gmail.com>
2023-05-22 17:28:47 +0200

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App(): JSX.Element {
+    return (
+        <div className="h-screen bg-black text-2xl text-white">
+            Devjobs-app React front setup
+        </div>
+    );
+}
