aba2e5d7b28415e6fcba3d780ecb3e2af7450f92
MikeeOo <michal.olszewskki@gmail.com>
2023-06-04 21:33:42 +0200

    added header to add-job-page

--- src/components/pages/AddJobPage.tsx
+++ src/components/pages/AddJobPage.tsx
@@ -1,39 +1,49 @@
 import { Card, Checkbox, Label, TextInput } from "flowbite-react";
+import Header from "../modules/Header";
 
 type Props = {};
 
 export default function AddJobPage({}: Props) {
     return (
-        <div>
-            <Card>
-                <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                    <p>Add a new job offer:</p>
-                </h5>
-                {/* <p className="font-normal text-gray-700 dark:text-gray-400">
+        <>
+            <Header />
+            <div className="mt-12">
+                <Card>
+                    <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                        <p>Add a new job offer:</p>
+                    </h5>
+                    {/* <p className="font-normal text-gray-700 dark:text-gray-400">
                         Here are the biggest enterprise technology acquisitions
                         of 2021 so far, in reverse chronological order.
-                </p> */}
-                <div>
-                    <div className="mb-2 block">
-                        <Label htmlFor="company-name" value="Company name" />
+                    </p> */}
+                    <div>
+                        <div className="mb-2 block">
+                            <Label
+                                htmlFor="company-name"
+                                value="Company name"
+                            />
+                        </div>
+                        <TextInput
+                            id="company-name"
+                            placeholder="The name of your company..."
+                            required
+                        />
                     </div>
-                    <TextInput
-                        id="company-name"
-                        placeholder="The name of your company..."
-                        required
-                    />
-                </div>
-                <div>
-                    <div className="mb-2 block">
-                        <Label htmlFor="company-name" value="Company webpage" />
+                    <div>
+                        <div className="mb-2 block">
+                            <Label
+                                htmlFor="company-name"
+                                value="Company webpage"
+                            />
+                        </div>
+                        <TextInput
+                            id="company-name"
+                            placeholder="Your company webpage..."
+                            required
+                        />
                     </div>
-                    <TextInput
-                        id="company-name"
-                        placeholder="Your company webpage..."
-                        required
-                    />
-                </div>
-            </Card>
-        </div>
+                </Card>
+            </div>
+        </>
     );
 }
